088b05d30f826f065fa59ecc8026f024d12c9f78
alan.zhao <alan.zhao@infosys.com>
2017-05-09 14:52:05 +0800

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {NavController, MenuController} from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, menu: MenuController) {
+    menu.enable(true);
+  }
+
+}
---
+++ 打包.cmd
@@ -0,0 +1,1 @@
+ionic build android --prod --release
